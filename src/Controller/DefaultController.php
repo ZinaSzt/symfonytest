611b0cc904d4241449143ff0cb3333150f5e69b3
ZinaSzt <iukwim404@gmail.com>
2023-05-17 10:18:51 +0200

    quete 3 debut

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/AppController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CustomAppController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'title' => 'Bienvenue !',
+        ]);
+    }
+}
